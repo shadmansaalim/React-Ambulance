599c3fda933b2d9ed150d5f5e9ffd630ed3ab345
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-18 02:56:03 +0600

    Faka JSON data created and fetched using useEffect

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('./services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div className="my-5">
-            <h1>sdcfsd dfsd</h1>
-            <h3>Hello adfad dfd</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, in hic ab illo sit debitis. Totam deleniti nostrum, nulla obcaecati velit nesciunt id, quia saepe maiores distinctio, delectus officiis voluptate.</p>
+
         </div>
     );
 };
