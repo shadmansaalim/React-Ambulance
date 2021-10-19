f8b6e66f0264906c807ad6d3f1a5a5941bbd71c2
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-19 20:58:31 +0600

    User Profile Button Centred

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -41,9 +41,9 @@ const Header = () => {
                         {
                             user ?
                                 user?.photoURL ?
-                                    <img className="img-fluid rounded-circle" src={user.photoURL} alt={user.displayName} style={{ width: 40, height: 40 }} data-bs-toggle="tooltip" data-bs-placement="bottom" title={user.displayName} onClick={handleShow} ></img>
+                                    <img className="mx-auto img-fluid rounded-circle" src={user.photoURL} alt={user.displayName} style={{ width: 40, height: 40 }} data-bs-toggle="tooltip" data-bs-placement="bottom" title={user.displayName} onClick={handleShow} ></img>
                                     :
-                                    <FontAwesomeIcon className="fs-1 text-white" data-bs-toggle="tooltip" data-bs-placement="bottom" title={user?.displayName} icon={faUserCircle} onClick={handleShow} />
+                                    <FontAwesomeIcon className="mx-auto  fs-1 text-white text-center" data-bs-toggle="tooltip" data-bs-placement="bottom" title={user?.displayName} icon={faUserCircle} onClick={handleShow} />
 
                                 :
                                 <div className="d-flex flex-column flex-lg-row mt-2 mt-lg-0">
