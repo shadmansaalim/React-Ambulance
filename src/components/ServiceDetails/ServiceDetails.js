f7ee192a667a9453fac8f7d8a6d83caeb09f99a3
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-20 13:35:53 +0600

    Details Page Updated

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -26,57 +26,55 @@ const ServiceDetails = () => {
                         <p>{selectedService.description}</p>
                     </div>
 
-                    <div className="d-flex justify-content-around mx-auto align-items-stretch my-5">
 
-                        <div className="card p-4 " style={{ backgroundColor: 'rgb(236, 239, 241)' }}>
-                            <article className="card-body mx-auto d-flex flex-column justify-content-center" style={{ maxWidth: '400px' }}>
+                    <section className="row d-flex mx-auto align-items-stretch mt-5">
+                        <div className="col-11 col-lg-6 mx-auto p-4 order-2 order-lg-1  mt-3 mt-lg-0 d-flex flex-column justify-content-center" style={{ backgroundColor: '#ECEFF1' }}>
 
-                                <p className="text-center">Provide Details and Confirm Booking</p>
+                            <p className="text-center mt-3">Provide Details and Confirm Booking</p>
 
-                                <form className="mt-4">
-                                    <div className="form-group input-group mb-3">
+                            <form className="mt-3 col-lg-10 mx-auto">
+                                <div className="form-group input-group mb-3 ">
+                                    <span className="input-group-text"> <i className="fa fa-user"></i> </span>
 
-                                        <span className="input-group-text"> <i className="fa fa-user"></i> </span>
+                                    <input name="" className="form-control" placeholder="Patient Name" type="text" required />
+                                </div>
+                                <div className="form-group input-group mb-3">
 
-                                        <input name="" className="form-control" placeholder="Patient Name" type="text" required />
-                                    </div>
-                                    <div className="form-group input-group mb-3">
+                                    <span className="input-group-text"> <i className="fa fa-envelope"></i> </span>
 
-                                        <span className="input-group-text"> <i className="fa fa-envelope"></i> </span>
+                                    <input name="" className="form-control" placeholder="Email Address" type="email" required />
+                                </div>
+                                <div className="form-group input-group mb-3">
 
-                                        <input name="" className="form-control" placeholder="Email Address" type="email" required />
-                                    </div>
-                                    <div className="form-group input-group mb-3">
+                                    <span className="input-group-text"> <i className="fa fa-home"></i> </span>
 
-                                        <span className="input-group-text"> <i className="fa fa-home"></i> </span>
+                                    <input name="" className="form-control" placeholder="Home Address" type="text" required />
+                                </div>
+                                <div className="form-group input-group mb-3">
 
-                                        <input name="" className="form-control" placeholder="Home Address" type="text" required />
-                                    </div>
-                                    <div className="form-group input-group mb-3">
+                                    <span className="input-group-text"> <i className="fa fa-phone"></i> </span>
 
-                                        <span className="input-group-text"> <i className="fa fa-phone"></i> </span>
-
-                                        <input name="" className="form-control" placeholder="Phone Number" type="number" required />
-                                    </div>
+                                    <input name="" className="form-control" placeholder="Phone Number" type="number" required />
+                                </div>
 
 
-                                    <div className="form-group my-4 text-center">
-                                        <button type="submit" className="btn btn-success btn-block">Confirm Booking</button>
-                                    </div>
+                                <div className="form-group my-4 text-center">
+                                    <button type="submit" className="btn btn-outline-success btn-block">Confirm Booking</button>
+                                </div>
 
-                                </form>
-                            </article>
+                            </form>
+
 
                         </div>
-                        <div>
-                            <h4>Why Choose {selectedService.name} ?</h4>
-                            <ul>
-                                {
-                                    (selectedService.advantages).map(advantage => <li>{advantage}</li>)
-                                }
-                            </ul>
-                        </div>
-                    </div>
+
+
+                        <img className="col-lg-6 mx-auto img-fluid rounded-3 order-1 order-lg-2" src={selectedService.image} alt="" />
+
+                    </section>
+
+
+
+
                 </section >
             }
         </div >
